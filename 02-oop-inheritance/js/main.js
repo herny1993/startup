17f9e4fd43b7e590ec30998737fa276b3e05fcfe
herny1993 <hernangrocha@yahoo.com.ar>
2014-06-23 18:17:36 -0300

    Added Downloadable Movie, Social mixin and Actors

--- 02-oop-inheritance/js/main.js
+++ 02-oop-inheritance/js/main.js
@@ -1,44 +1,134 @@
 var Movie = (function(){
-  // Attributes
-  var attributes = {};
 
   // Constructor
-  function Movie() {}
+  function Movie() {    
+    // Attributes
+    var attributes = {};
+    var actors = new Array();
+
+    this.set = function(attr, value){
+      attributes[attr] = value;
+    }
+
+    this.get = function(attr){
+      return attributes[attr];
+    }
+
+    this.addActor = function(actor){
+      actors[actors.length] = actor;
+    }
+
+    this.showActors = function(){
+      console.log("Actors of " + this.get("title") + ":");
+      for (var i in actors){
+        console.log(" - " + actors[i].getName());
+      }
+    }
+  }
 
   // Methods
+  Movie.prototype.constructor = Movie;
+
   Movie.prototype.play = function(){   
     $(this).trigger("playing");
+
+    return this;
   }
 
   Movie.prototype.stop = function(){
     $(this).trigger("stopped");
-  }
 
-  Movie.prototype.set = function(attr, value){
-    attributes[attr] = value;
-  }
-
-  Movie.prototype.get = function(attr){
-    return attributes[attr];
+    return this;
   }
 
   return Movie;
 })();
 
+/* -------------------- MOVIE OBSERVER -------------------- */
+
 var MovieObserver = function(movie){
   $(movie).on("playing", function(){
-    console.log("Playing " + this.get("title") + " from MovieObserver");
+    console.log("[MovieObserver] Playing " + this.get("title"));
   });
 
   $(movie).on("stopped", function(){
-    console.log("Stopped " + this.get("title") + " from MovieObserver");
+    console.log("[MovieObserver] Stopped " + this.get("title"));
   });
+}
+
+/* -------------------- DOWNLOADABLE MOVIE -------------------- */
+
+function DownloadableMovie(title){
+  this.constructor(title);
+}
+
+DownloadableMovie.prototype = new Movie();
+
+DownloadableMovie.prototype.download = function(){
+  console.log("Downloading " + this.get("title"));
+
+  return this;
+};
+
+
+/* -------------------- MIXIN -------------------- */
+
+function mixin(dest, source) {
+  for (var i in source) {
+    if (source.hasOwnProperty(i)) {
+      dest[i] = source[i];
+    }
+  }
+
+  return dest;
+}
+
+var Social = {
+  share: function(friendName){
+    console.log("Sharing " + this.get("title") + " with " + friendName);
+  },
+
+  like: function(){
+    console.log("Liked " + this.get("title"));
+  }
+};
+
+// Mixin
+mixin(Movie.prototype, Social);
+
+
+/* -------------------- ACTOR -------------------- */
+
+var Actor = function(name){
+  var name = name;
+
+  this.getName = function (){
+    return name;
+  }
 }
 
 var titanic = new Movie();
 var titanicObserver = new MovieObserver(titanic);
+var terminator = new Movie();
+var terminatorObserver = new MovieObserver(terminator);
+
+// Downloadable
+var thor = new DownloadableMovie();
+var thorObserver = new MovieObserver(thor);
+
+// Actors
+var leonardo = new Actor("Leonardo DiCaprio");
+var kate = new Actor("Kate Winslet");
 
 $(document).ready(function(){
   titanic.set("title", "Titanic");
+  terminator.set("title", "Terminator");
+  thor.set("title", "Thor");
   titanic.play();
+  terminator.stop();
+
+  // Actors
+  titanic.addActor(leonardo);
+  titanic.addActor(kate);
+  titanic.showActors();
 });
