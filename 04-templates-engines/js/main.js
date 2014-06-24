866a8034304be04b0c62bced93bb97cc019f0896
herny1993 <hernangrocha@yahoo.com.ar>
2014-06-24 04:32:39 -0300

    Underscore and Dust

--- 04-templates-engines/js/main.js
+++ 04-templates-engines/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	
+
 	// Compilation
 	var source = $("#entry-template").html();
 	var template = Handlebars.compile(source);
@@ -11,4 +11,15 @@ $(document).ready(function(){
 	var target = $("#entry");
 	target.html(html);
 
+	// Underscore
+	var numbers = [10, 5, 100, 2, 1000];
+	console.log("Minimum: " + _.min(numbers));
+
+	// Dust
+	var compiled = dust.compile("<div class='intro'> Hello {name}! </div>", "intro");
+	console.log(compiled);
+	//dust.render(compiled(), {name: "Fred"}, function(err, out) {
+  	//	console.log("Salida: " + out);
+  	//	console.log("Error: " + err);
+	//});
 });
